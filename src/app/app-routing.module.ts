6ea29cf88439ceba807e4463289110c69d69912e
aymen2782 <aymen.sellaouti@gmail.com>
2023-08-04 11:30:20 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { FirstComponent } from './first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: FirstComponent},
+  { path: 'test', component: TestComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-pwa-app';
+  isDownladable = false;
+  deferredPrompt!: Event | null;
+  constructor(private swUpdate: SwUpdate) {
+    addEventListener('beforeinsatallprompt', (e) => {
+      e.preventDefault();
+      this.deferredPrompt = e;
+      this.isDownladable = true;
+    });
+  }
+  ngOnInit(): void {
+    this.swUpdate.versionUpdates.subscribe((version) => {
+      if (version.type === 'VERSION_READY') {
+        if (
+          confirm(
+            `Une nouvelle version de l'application est disponionible, vouslez vous recharger la page ?`
+          )
+        ) {
+          window.location.reload();
+        }
+      }
+    });
+  }
+  download() {
+    // if (this.deferredPrompt) {
+    //   this.deferredPrompt.prompt();
+    //   this.deferredPrompt = null;
+    // }
+  }
 }
